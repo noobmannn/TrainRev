5e5098c9e8be96b79ccd41c53e7a61f419f4b984
noobmannn <102444334+noobmannn@users.noreply.github.com>
2024-05-28 21:29:26 +0700

    Create Evil.c

---
+++ Process_Hollowing/Src/Evil.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+#include <WinUser.h>
+
+int main() {
+    MessageBoxA(NULL, (LPCSTR)"THIS PROCESS HAS BEEN INJECTED BY NOOBMANNN!!!!", (LPCSTR)"WARNING!!!", MB_OK);
+    return 0;
+}
